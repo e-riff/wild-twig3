ee55e5901d55f34792927f3faca9a315577a0dd6
e-riff <emeric.riff@gmail.com>
2022-10-19 10:43:40 +0200

    twig3 ok

--- public/details.php
+++ public/details.php
@@ -3,5 +3,4 @@
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
 
-$name = 'MrNem';
-echo $twig->render('home.html.twig', ['name' => $name]);
+echo $twig->render('details.html.twig');
--- public/home.php
+++ public/home.php
@@ -1,11 +1,7 @@
 <?php
 
-
-
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
 
-
 $products = ['mandoline', 'bass', 'banjo', 'cithare', 'lyre', 'harpe'];
-
 echo $twig->render('home.html.twig', ['products' => $products]);
