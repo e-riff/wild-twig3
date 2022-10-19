beff2cc558661a6586fc14d1088e15e0cb9f8415
e-riff <emeric.riff@gmail.com>
2022-10-19 09:21:58 +0200

    update twig2

--- config/twig.php
+++ config/twig.php
@@ -5,3 +5,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+
+
+//Mes globales
+$contactEmail = 'mrnem@vivaldi.fr';
+$twig->addGlobal('contactEmail', $contactEmail);
